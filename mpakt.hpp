73c4ed0020860d4e766fdf5c1c93740a7679257f
Eric T. Dawson <eric.t.dawson@gmail.com>
2019-03-14 16:23:29 -0400

    Fix return vals of functions

--- mpakt.hpp
+++ mpakt.hpp
@@ -12,6 +12,7 @@ const static std::uint32_t MPAKT_V0_MAGIC_NUMBER = 4242420;
 inline std::ostream& write_c_string(std::ofstream& os, std::size_t len, const char* x){
     os.write(reinterpret_cast<char*>(&len), sizeof(std::size_t));
     os.write(x, len);
+    return os;
 };
 
 inline std::istream& read_c_string(std::istream& is, char*& x){
@@ -20,6 +21,7 @@ inline std::istream& read_c_string(std::istream& is, char*& x){
     x = new char[sz+1];
     is.read(x, sz);
     x[sz] = '\n';
+    return is;
 };
 
 template<typename X>
@@ -35,32 +37,32 @@ inline std::ostream& write_field(std::ostream& os, const X* x){
 }
 
 template<typename X>
-inline void read_field(std::istream& is, X& x){
+inline std::istream& read_field(std::istream& is, X& x){
     is.read(reinterpret_cast<char*>(&x), sizeof(X));
+    return is;
 };
 
 template<typename X>
-inline void read_field(std::istream&is, X*& x){
+inline std::istream& read_field(std::istream&is, X*& x){
+    x = new X;
     is.read(reinterpret_cast<char*>(&x), sizeof(X));
+    return is;
 };
 
 template<typename X>
-inline std::ostream& write_array(std::ostream& os, std::size_t count, const X* data){
+inline std::ostream& write_array(std::ostream& os, std::size_t count, const X*& data){
     os.write((char*)&count, sizeof(count));
-    for (std::size_t i = 0; i < count; ++i){
-        os.write(reinterpret_cast<char*>(&data[i]), sizeof(X));
-    }
+    os.write(reinterpret_cast<char*>(&data), count * sizeof(X));
     return os;
 };
 
 template<typename X>
-inline void read_array(std::istream& is, X*& data){
-    std::size_t sz;
+inline std::istream& read_array(std::istream& is, X*& data){
+    std::size_t sz = 0;
+    is.read(reinterpret_cast<char*>(&sz), sizeof(std::size_t));
     data = new X[sz];
-    is.read((char*)sz, sizeof(std::size_t));
-    for (std::size_t i = 0; i < sz; ++i){
-        is.read(reinterpret_cast<char*>(&data[i]), sizeof(X));
-    }
+    is.read(reinterpret_cast<char*>(&data), sz * sizeof(X));
+    return is;
 };
 
 
